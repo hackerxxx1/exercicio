a0e916acab18ba8a9980ac874a3f246704fb99c2
hackerxxx1 <43994798+hackerxxx1@users.noreply.github.com>
2022-03-10 18:33:24 -0300

    atualização branch do exercicio
    algumas adaptações e desenvolvimento do check email

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function cliquei() {
     alert('aperta ok ai vai vc vai gostar');
 }
+
+function check(email) {
+    if (email.toLowerCase().endsWith('@yahoo.com') || email.toLowerCase().endsWith('@gmail.com') || email.toLowerCase().endsWith('@yahoo.com')) {
+        alert('email valido aperte ok por favor')
+    } else {
+        alert('email invalido ou fora dos dominios suportados')
+    }
+}
+$(document).ready(
+    function() {
+        alert("bem vindo")
+    })
